f2972dfff044ac017784af0e2099fc1ceedc7420
shashankb-cc <163817665+shashankb-cc@users.noreply.github.com>
2024-04-03 17:54:17 +0530

    today

--- New folder/linked-list.js
+++ New folder/linked-list.js
@@ -1,69 +1,198 @@
-import assert from 'assert';
+import assert from "assert";
+import { isEqual } from "lodash";
 
-export function createList() {
-  return { head: null };
+export function createList(arg) {
+  assert(
+    arg === undefined || Array.isArray(arg) || arg instanceof Object,
+    "The argument must be undefined or array or list "
+  );
+  if (arg === undefined) {
+    //if we call without any arguments
+    return { head: null, tail: null };
+  } else if (Array.isArray(arg)) {
+    //if we call this with array as an argument
+    let head = null;
+    let tail = null;
+    for (let i = arg.length - 1; i >= 0; i--) {
+      const newNode = { data: arg[i], next: head };
+      head = newNode;
+      if (tail === null) {
+        tail = newNode;
+      }
+    }
+    return { head, tail };
+  } else {
+    //if we call this with list as an argument
+    let newList = { head: null, tail: null };
+    let head = null;
+    let tail = null;
+    let currentNode = arg.head;
+
+    while (currentNode !== null) {
+      const newNode = { data: currentNode.data, next: null };
+      if (head === null) {
+        head = newNode;
+        newList.head = head;
+        tail = newNode;
+        newList.tail = tail;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+        newList.tail = tail;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return newList;
+  }
 }
 /**
- * adds data to end of thr list
+ * adds data to end of the list
  */
-export function addItemToList(listRef,data) {
-    //assert(listRef !==undefined && listRef !==null);
-    const newNode={data:data,next:null};
-    if(listRef.head===null){
-        listRef.head=newNode;
-    }else{
-        //traverse until last node  and link the new node it
-        let lastNode=listRef.head;
-        while(lastNode.next !==null){
-            lastNode=lastNode.next;
-        }
-        lastNode.next=newNode;
-    }
-    return newNode;
+export function addItemToList(listRef, data) {
+  assert(
+    listRef !== undefined && listRef !== null,
+    "You must have atleast one node to add item to list"
+  );
+  assert(data !== null, "Data should not be empty");
+  const newNode = { data: data, next: null };
+  if (listRef.head === null) {
+    listRef.head = newNode;
+    listRef.tail = newNode;
+  } else {
+    listRef.tail.next = newNode;
+    listRef.tail = newNode;
+  }
+  return newNode;
 }
 
-export function arrayFromList(listRef){
-    const array=[];
-    let lastNode=listRef.head;
-        while(lastNode.next !==null){
-            array.push(lastNode.data);
-            lastNode=lastNode.next;
-            
+export function arrayFromList(listRef) {
+  assert(
+    listRef !== undefined && listRef !== null,
+    "You must have items in list to convert it into array"
+  );
+  const array = [];
+  let lastNode = listRef.head;
+  while (lastNode.next !== null) {
+    array.push(lastNode.data);
+    lastNode = lastNode.next;
+  }
+  array.push(lastNode.data);
+  return array;
+}
+export function removeFromEnd(listRef) {
+  assert(
+    listRef !== undefined && listRef !== null && listRef.head !== null,
+    "There are no elements in the list to remove"
+  );
+  let currentNode = listRef.head;
+  let previousNode = null;
+  while (currentNode.next !== null) {
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  if (previousNode === null) {
+    // if prevNode is null means it has only one node because it does not enter the loop
+    listRef.head = null;
+    listRef.tail = null;
+  } else {
+    previousNode.next = null;
+    listRef.tail = previousNode;
+  }
+  return currentNode.data;
+}
+
+export function removeSpecificItem(listRef, nodeToRemove) {
+  assert(
+    listRef !== undefined && listRef !== null,
+    "There are no elements in the list to remove"
+  );
+  let previousNode = null;
+  let currentNode = listRef.head;
+  if (currentNode.next === null) {
+    listRef.head = null;
+    listRef.tail = null;
+    return currentNode.data;
+  } else {
+    while (currentNode !== null) {
+      if (currentNode === nodeToRemove) {
+        previousNode.next = currentNode.next;
+        if (currentNode.next === null) {
+          // we have reached last node
+          listRef.tail = previousNode;
         }
-        array.push(lastNode.data);
-        return array;
-}
-export function removeFromEnd(listRef){
-    let lastNode=listRef.head;
-    let prevNode=null;
-        while(lastNode.next !==null){
-            prevNode=lastNode;
-            lastNode=lastNode.next;
-        }
-        prevNode.next=null;
-        return lastNode.data;
-}
-export function insertAfter(listRef,listNode,data){
-    let newNode={data:data,next:null}
-    let currentNode=listRef.head;
-    let nextNode=currentNode.next;
-    while(currentNode.data !== listNode.data){
-        currentNode=currentNode.next;
-        nextNode=currentNode.next;
+        return currentNode.data;
+      }
+      previousNode = currentNode;
+      currentNode = currentNode.next;
     }
-    currentNode.next=newNode;
-    newNode.next=nextNode;
-    return newNode;
+  }
 }
-export function insertBefore(listRef,listNode,data){
-    let newNode={data:data,next:null};
-    let previousNode=null;
-    let currentNode=listRef.head;
-    while(currentNode.data !== listNode.data){
-        previousNode=currentNode;
-        currentNode=currentNode.next;
+
+export function insertAfter(listRef, listNode, data) {
+  assert(
+    listRef !== undefined && listRef !== null,
+    "You must have atleast one node to add item to list"
+  );
+  assert(
+    listNode !== undefined || listNode !== null,
+    "You must define the position of node correctly"
+  );
+  assert(data !== null, "You must define what data must be added");
+
+  let currentNode = listRef.head;
+
+  while (currentNode !== null) {
+    if (currentNode === listNode) {
+      let newNode = { data: data, next: null };
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+      return newNode;
     }
-    previousNode.next=newNode;
-    newNode.next=currentNode;
-    return newNode;
+    currentNode = currentNode.next;
+  }
 }
+
+// export function insertBefore(listRef, listNode, data) {
+//   assert(
+//     listRef !== undefined && listRef !== null,
+//     "You must have atleast one node to add item to list"
+//   );
+//   assert(
+//     listNode !== undefined || listNode !== null,
+//     "You must define the position of node correctly"
+//   );
+//   assert(data !== null, "You must define what data must be added");
+
+//   let previousNode = null;
+//   let currentNode = listRef.head;
+
+//   // If single node
+//   if (currentNode === listNode) {
+//     let newNode = { data: data, next: currentNode };
+//     listRef.head = newNode;
+//     return newNode;
+//   }
+
+//   while (currentNode) {
+//     if (currentNode === listNode) {
+//       let newNode = { data: data, next: currentNode };
+//       previousNode.next = newNode;
+//       return newNode;
+//     }
+//     previousNode = currentNode;
+//     currentNode = currentNode.next;
+//   }
+// }
+
+// export function filterList(listRef, isNumber) {
+//   assert(
+//     listRef !== undefined && listRef !== null,
+//     "You must have atleast one node to add item to list"
+//   );
+//   const resultArr = arrayFromList(listRef);
+//   return resultArr.filter((nodedata) => isNumber(nodedata));
+// }
+// export function isNumber(data) {
+//   return typeof data === "number";
+// }
--- New folder/linked-list.test.js
+++ New folder/linked-list.test.js
@@ -4,45 +4,132 @@ import {
   addItemToList,
   arrayFromList,
   removeFromEnd,
+  removeSpecificItem,
   insertAfter,
   insertBefore,
+  filterList,
+  isNumber,
 } from "./linked-list";
 
-test("Linked list tests", () => {
-  //Test creation of a linked list
+/**
+ *Tests for List creation using createList() with arguments
+ * no argument
+ *array as argument
+ *list as an argument
+ */
+
+test("Linked list createList tests", () => {
+  // Test creation of a linked list without an argument
   const listRef = createList();
   expect(listRef).toBeDefined();
   expect(listRef.head).toBeNull();
+  expect(listRef.tail).toBeNull();
 
-  //Test addition of items into the list
+  // Test creation of a linked list with an array as an argument
+  const listRefArray = createList([1, "two", { three: 3 }, 4.0]);
+  expect(listRefArray).toBeDefined();
+  expect(listRefArray.head.data).toBe(1);
+  expect(listRefArray.tail.data).toBe(4.0);
+  expect(listRefArray).toEqual({
+    head: {
+      data: 1,
+      next: {
+        data: "two",
+        next: {
+          data: { three: 3 },
+          next: {
+            data: 4.0,
+            next: null,
+          },
+        },
+      },
+    },
+    tail: {
+      data: 4.0,
+      next: null,
+    },
+  });
+
+  // Test creation of a linked list with another list as an argument
+  const listRefList = createList({
+    head: {
+      data: "one",
+      next: {
+        data: "two",
+        next: {
+          data: { three: 3 },
+          next: {
+            data: 4.0,
+            next: null,
+          },
+        },
+      },
+    },
+  });
+  expect(listRefList).toBeDefined();
+  expect(listRefList.head.data).toBe("one");
+  expect(listRefList.tail.data).toBe(4.0);
+  expect(listRefList).toEqual({
+    head: {
+      data: "one",
+      next: {
+        data: "two",
+        next: {
+          data: { three: 3 },
+          next: {
+            data: 4.0,
+            next: null,
+          },
+        },
+      },
+    },
+    tail: {
+      data: 4.0,
+      next: null,
+    },
+  });
+});
+
+/**
+ * Tests for adding a item into list
+ */
+
+test("Linked list add,delete,toArray conversion tests", () => {
+  const listRef = createList();
+
+  // Add first item
   const listNode1 = addItemToList(listRef, 1);
   expect(listNode1).toBeDefined();
-
-  //since this was first node,we expect listRef.head must be
-  //equal to listNode we just added
   expect(listRef.head).toBe(listNode1);
-
+  expect(listRef.tail).toBe(listNode1);
   expect(listNode1.data).toBe(1);
-  expect(listNode1.next).toBe(null);
+  expect(listNode1.next).toBeNull();
 
-  //Add another item
+  //some more items
+
   const listNode2 = addItemToList(listRef, "two");
   expect(listNode2.data).toBe("two");
-  expect(listNode2.next).toBe(null);
+  expect(listNode2.next).toBeNull();
   expect(listNode1.next).toBe(listNode2);
+  expect(listRef.head).toBe(listNode1);
+  expect(listRef.tail).toBe(listNode2);
 
   const listNode3 = addItemToList(listRef, { name: "shashank", age: 22 });
   expect(listNode3.data).toEqual({ name: "shashank", age: 22 });
-  expect(listNode3.next).toBe(null);
+  expect(listNode3.next).toBeNull();
   expect(listNode2.next).toBe(listNode3);
+  expect(listRef.head).toBe(listNode1);
+  expect(listRef.tail).toBe(listNode3);
 
   const listNode4 = addItemToList(listRef, [0, 1]);
   expect(listNode4.data).toEqual([0, 1]);
-  expect(listNode4.next).toBe(null);
+  expect(listNode4.next).toBeNull();
   expect(listNode3.next).toBe(listNode4);
+  expect(listRef.head).toBe(listNode1);
+  expect(listRef.tail).toBe(listNode4);
 
   //lets test list can give as array representation
-
+  //This converts list to array format and returns it
   let listAsArray = arrayFromList(listRef);
   expect(listAsArray).toEqual([
     1,
@@ -51,23 +138,41 @@ test("Linked list tests", () => {
     [0, 1],
   ]);
 
-  //Removing elements from end
+  /**
+   *Removing an element from end of the list
+   */
   const removedItem = removeFromEnd(listRef);
   expect(removedItem).toEqual([0, 1]);
+  expect(listRef.tail.data).toEqual({ name: "shashank", age: 22 });
+  expect(listRef.tail.next).toBeNull();
 
-  //Removing specific element thats is passed as argument
+  /**
+   * Removing a element from thats passed as an argument
+   */
+  const removeSpecificNode = removeSpecificItem(listRef, listNode3);
+  expect(removeSpecificNode).toEqual({ name: "shashank", age: 22 });
+  expect(listNode2.next).toBe(null);
 
-  //Inserting a node after a specific node
+  //   /**
+  //    * Inserting a node after a specific node passed as an argument
+  //    */
+  //   const listNodeAfter = insertAfter(listRef, listNode2, "three");
+  //   expect(listNodeAfter.data).toEqual("three");
+  //   expect(listNode2.next).toBe(listNodeAfter);
 
-  const listNodeAfter = insertAfter(listRef, listNode2, "three");
-  expect(listNodeAfter.data).toEqual("three");
-  expect(listNodeAfter.next).not.toBe(null);
-  expect(listNode2.next).toBe(listNodeAfter);
+  //   /**
+  //    * Inserting a node before a specific node passes as an argument
+  //    */
 
-  //Inserting a node before a specific node
+  //   const listNodeBefore = insertBefore(listRef, listNode2, 2);
+  //   expect(listNodeBefore.data).toEqual(2);
+  //   expect(listNodeBefore.next).not.toBe(null);
+  //   expect(listNode1.next).toBe(listNodeBefore);
 
-  const listNodeBefore = insertBefore(listRef, listNode2, "two-2");
-  expect(listNodeBefore.data).toEqual("two-2");
-  expect(listNodeBefore.next).not.toBe(null);
-  expect(listNode1.next).toBe(listNodeBefore);
+  //   /**
+  //    * Checking whether the data of listNode pass the predicate function logic
+  //    */
+  //   const resultArr = filterList(listRef, isNumber);
+  //   expect(resultArr).toEqual([1, 2]);
+  //   expect(resultArr).not.toBe(null);
 });
